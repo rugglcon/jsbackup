0fcdbef069d6a8b0aad4345a46a1c785dcecb95b
cruggles <cruggles@iastate.edu>
2018-08-02 19:41:17 -0500

    start as a backup utility

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,19 @@ import * as fs from 'fs';
 import * as tar from 'tar';
 import * as path from 'path';
 
-tar.create({
-    gzip: true,
-    file: 'out.tar.gz'
-}, [path.join(os.homedir(), '.vim/vimrc')])
-.then(() => {
-    return tar.extract({
-        file: 'out.tar.gz'
-    });
-})
-.then(() => {
-    console.log('yay');
+// TODO: have this take in arguments for what
+// to compress and where to put it, if anywhere
+process.chdir(path.join(os.homedir(), 'cur-class'));
+const items = fs.readdirSync(process.cwd());
+let tars = [];
+items.forEach(element => {
+    if (fs.lstatSync(element).isDirectory()) {
+        let prom = tar.create({
+            gzip: true,
+            file: `${element}.tar.gz`
+        }, [element]);
+        console.log('compressing', element);
+        tars.push(prom);
+    }
 });
+Promise.all(tars).then(() => console.log('done.'));
