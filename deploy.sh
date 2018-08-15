b6240bf2580e0b0149c9f9048612f8923be48198
cruggles <cruggles@iastate.edu>
2018-08-14 21:28:30 -0500

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+TAG=""
+MESSAGE=""
+
+npm run ci
+if [ $? -neq 0 ]; then
+    "Build failed. Exiting."
+done
+
+echo "Deploy process started"
+echo ""
+
+echo "Did you update the version in package.json?"
+read -p "Press [Enter] if you did."
+
+read -e -p 'Enter tag: ' TAG
+read -e -p 'Enter message: ' MESSAGE
+
+git tag -a "v$TAG" -m "$MESSAGE"
+git push --tags
+
+npm publish
